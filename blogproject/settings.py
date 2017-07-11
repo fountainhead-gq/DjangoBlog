440cb046fa695540d262d00531192a678d80a440
fountainhead-gq <fountainhead_gq@163.com>
2017-07-11 17:57:08 +0800

    modify blog

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -45,7 +45,28 @@ INSTALLED_APPS = [
     'nocaptcha_recaptcha',
     'import_export',
     'blog',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ),
+    # 'DEFAULT_PARSER_CLASSES': (
+    #     'rest_framework.parsers.JSONParser',
+    # )
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+         #'rest_framework.authentication.SessionAuthentication',
+         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        #'rest_framework.authentication.BasicAuthentication'
+
+    ), 
+    "DEFAULT_PERMISSION_CLASSES": (
+        'rest_framework.permissions.IsAuthenticated',
+        #'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    )
+}
 
 # Django Suit configuration
 SUIT_CONFIG = {
