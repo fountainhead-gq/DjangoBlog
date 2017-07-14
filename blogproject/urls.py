76127f40a886db8303f5821db7430e5fc93b186e
fountainhead-gq <fountainhead_gq@163.com>
2017-07-14 14:50:23 +0800

    urls

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 
-from django.conf.urls import (
-    handler400, handler403, handler404, handler500
-)
+from django.conf.urls import (handler400, handler403, handler404, handler500)
+
 handler400 = 'blog.views.handler400'
 handler403 = 'blog.views.handler403'
 handler404 = 'blog.views.handler404'
@@ -28,6 +27,6 @@ handler500 = 'blog.views.handler500'
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^redactor/', include('redactor.urls')),
+    # url(r'^redactor/', include('redactor.urls')),
     url(r'^', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
